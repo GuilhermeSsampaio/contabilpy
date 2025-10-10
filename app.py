5ba5728ef1a70f08ee8baa56594f349b128869fd
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-10-09 20:40:27 -0400

    feat: novo app

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ load_dotenv()
 st.header("_:blue[ContabiliPy]_ - velocidade na contabilidade", divider=True)
 
 sidebar()
+st.title("Bem-vindo ao ContabiliPy")
+st.write("Sua plataforma de contabilidade automatizada com IA e um arcabouço completo de ferramentas.")
+st.write("Escolha uma das opções no menu lateral para começar.")
+st.write("Desenvolvido por [@Guilherme Sampaio](https://www.linkedin.com/in/guilhermessampaio)")
